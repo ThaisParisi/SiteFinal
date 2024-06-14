90217fd3d772a7eca3d72f66505c504b57cdabc2
parisi <160138803+parisilindona@users.noreply.github.com>
2024-06-14 14:32:23 -0300

    Add files via upload

---
+++ criptografia final/assets/js/main.js
@@ -0,0 +1,37 @@
+const letraA = ['A','a',0,0];
+const letraB = ['B','b',1,0];
+const letraC = ['C','c',2,0];
+const letraD = ['D','d',3,0];
+const letraE = ['E','e',4,0];
+
+const letraF = ['F','f',0,1];
+const letraG = ['G','g',1,1];
+const letraH = ['H','h',2,1];
+const letraI = ['I','i',3,1];
+const letraJ = ['J','j',4,1];
+
+const letraK = ['K','k',0,2];
+const letraL = ['L','l',1,2];
+const letraM = ['M','m',2,2];
+const letraN = ['N','n',3,2];
+const letraO = ['O','o',4,2];
+
+const letraP = ['P','p',0,3];
+const letraQ = ['Q','q',1,3];
+const letraR = ['R','r',2,3];
+const letraS = ['S','s',3,3];
+const letraT = ['T','t',4,3];
+
+const letraU = ['U','u',0,4];
+const letraV = ['V','v',1,4];
+const letraW = ['W','w',2,4];
+const letraX = ['X','x',3,4];
+const letraY = ['Y','y',4,4];
+
+const letraZ = ['Z','z',0,5];
+const espaço = [' ',1,5];
+const pFinal = ['.',2,5];
+const virgula = [',',3,5];
+const interroga = ['?',4,5];
+
+const alfabeto = [letraA,letraB,letraC,letraD,letraE,letraF,letraG,letraH,letraI,letraJ,letraK,letraL,letraM,letraN,letraO,letraP,letraQ,letraR,letraS,letraT,letraU,letraV,letraW,letraX,letraY,letraZ,espaço,pFinal,virgula,interroga];
